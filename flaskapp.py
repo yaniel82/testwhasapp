d6bde6b9048a5f62b09a290c8ff1ee8ff6d59db2
yaniel82 <42359817+yaniel82@users.noreply.github.com>
2018-08-13 18:20:02 -0400

    Add files via upload

---
+++ flaskapp.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+
+#WA[
+from whatsapp import Client
+expected_token = 'juQ6uLqa8mC50Hi8eAClgYr8dow='
+#]WA
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+
+    return 'Hello World!'
+
+@app.route('/msg', methods = ['POST'])
+def msg():
+    to = request.form['to']
+    return str(to)
+
+@app.route('/sendmsg')
+def sendmsg():
+    to = request.args.get('to')
+    msg = request.args.get('msg')
+    token = request.args.get('token')
+    if(str(token) == expected_token):
+        client = Client(login='5352181471', password='fPD9Ds4TBeKqHO6XfrM8gPcDb2o=')
+        res = client.send_message(to, msg)  
+    
+    else:
+        res = 'Unauthorized'
+    
+    return str(res)
+
+if __name__ == '__main__':
+    app.run()
